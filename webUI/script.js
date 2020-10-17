506ef43d3568e585d529a8eb2cfae65c89904c43
Luke1024 <chajdas.lukasz@gmail.com>
2020-10-17 14:14:38 +0200

    problem with form submitting solved

--- webUI/script.js
+++ webUI/script.js
@@ -1,17 +1,30 @@
 $(document).ready(function(){
+
+  let appContainer = $("[app-container]");
+  let cardContainer = $("[card-container]");
+  let cardTemplate = $("[card-area-template]");
+
   console.log("execution");
+  loadLayout();
 
-  let cardArea = $("#cardArea");
-
-
-
-
-  function cardEditModeToggle(){
-      let card = $('#cardEditMode');
-      card.slideToggle(500);
-      console.log(card);
+  function loadLayout(){
+    let template = cardTemplate.children('[card]');
+    let newCard = template.clone();
+    newCard.children("[new-card]").removeClass('hidden');
+    newCard.appendTo(cardContainer);
   }
 
-  cardArea.on('click', '#cardCancel', cardEditModeToggle);
-  cardArea.on('click', '#newTaskButton', cardEditModeToggle);
+  function cardEditModeToggle(){
+    let newCard = $(this).parents('[new-card]');
+    let cardEdit = newCard.siblings('[card-edit-mode]');
+    cardEdit.slideToggle(500);
+  }
+
+  function cardEditCancel(){
+    let cardEdit = $(this).parents('[card-edit-mode]');
+    cardEdit.slideUp();
+  }
+
+  appContainer.on('click', '[card-cancel]', cardEditCancel);
+  appContainer.on('click', '[new-task-button]', cardEditModeToggle);
 });
