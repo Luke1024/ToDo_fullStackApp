be36b60b7600d72347a24be9620654fa0a2ae961
Luke1024 <chajdas.lukasz@gmail.com>
2020-11-02 16:59:35 +0100

    card menu concept change

--- webUI/script.js
+++ webUI/script.js
@@ -1,8 +1,22 @@
 $(document).ready(function(){
-
   let appContainer = $("[app-container]");
   let cardContainer = $("[card-container]");
   let cardTemplate = $("[card-area-template]");
+
+  //card variables
+  let cardId;
+  let cardMode; 
+  let form;
+  let nameForm;
+  let descriptionForm;
+  let paragraphs;
+  let nameParagraph;
+  let descriptionParagraph;
+  let cardFolded;
+  let buttonSave;
+  let buttonEdit;
+  let buttonUpdate;
+  let buttonCancel;
 
   console.log("execution");
   loadLayout();
@@ -16,46 +30,27 @@ $(document).ready(function(){
     let newCard = template.clone();
     newCard.appendTo(cardContainer);
   }
+/*
+  function loadCardPartsToGlobalVariables(button){
+    let card = button.parents('[card]');
 
-  function unfoldCard(){
-    let cardEdit = $(this).siblings('[card-edit-mode]');
-    let cardFolded = cardEdit.siblings('[card-folded]');
-    cardFolded.addClass("hidden");
-    cardEdit.slideDown(100);
-  }
+    cardId = card.children('[card-id]');
+    cardMode = card.children('[card-mode]'); 
 
-  function saveCard(){
-    let cardEdit = $(this).parents('[card-edit-mode]');
-    let form = $(this).siblings('[task-form]');
-    let name = form.children('[task-name]');
-    let description = form.children('[task-description]');
-    let nameContent = $(this).siblings('[task-name-content]');
-    let descriptionContent = $(this).siblings('[task-description-content]');
-    let cardFolded = cardEdit.siblings('[card-folded]');
-    if(name.val().length > 1){
-      form.addClass('hidden');
-      nameContent.text(name.val());
-      descriptionContent.text(description.val());
-      switchToEditButton($(this));
-      cardFolded.children('[content]').text(name.val());
-      loadCardTemplate();
-      cardEdit.slideUp(100);
-      cardFolded.removeClass('hidden');
-    } else {
-      description.val("");
-      cardFolded.removeClass('hidden');
-      cardEdit.slideUp(100);
-    }
-  }
+    form = card.children('[task-form]');
+    nameForm= card.children('[task-name-form]');
+    descriptionForm = card.children('[task-description-form]');
 
-  function switchToEditButton(button){
-    button.addClass("hidden");
-    button.siblings('[card-edit]').removeClass("hidden");
-  }
+    paragraphs = card.children('[paragraphs]');
+    nameParagraph = card.children('[task-name-paragraph]');
+    descriptionParagraph = card.children('[task-description-paragraph]');
 
-  function switchToUpdateButton(button){
-    button.addClass("hidden");
-    button.siblings('[card-update]').removeClass("hidden");
+    cardFolded = card.children('[card-folded]');
+
+    buttonSave = card.children('[button-card-save]');
+    buttonEdit = card.children('[button-card-edit]');
+    buttonUpdate = card.children('[button-card-update]');
+    buttonCancel = card.children('[button-card-cancel]');
   }
 
   function cardEditCancel(){
@@ -69,6 +64,22 @@ $(document).ready(function(){
     form.addClass('hidden');
     cardFolded.removeClass('hidden');
     cardEdit.slideUp(100);
+  }
+
+  function saveCard(){
+    loadCardPartsToGlobalVariables($(this));
+    if(nameForm.val().length > 1){
+      loadTextFromFormToParagraph();
+      cardFolded.children('[content]').text(nameForm.val());
+      switchToSavedMode();
+      loadCardTemplate();
+      cardEdit.slideUp(100);
+      cardFolded.removeClass('hidden');
+    } else {
+      descriptionForm.val("");
+      cardFolded.removeClass('hidden');
+      cardEdit.slideUp(100);
+    }
   }
 
   function editCard(){
@@ -107,10 +118,15 @@ $(document).ready(function(){
       cardEdit.slideUp(100);
     }
   }
+  */
 
-  appContainer.on('click', '[card-cancel]', cardEditCancel);
-  appContainer.on('click', '[card-save]', saveCard);
-  appContainer.on('click', '[card-edit]', editCard);
-  appContainer.on('click', '[card-update]', cardUpdate);
-  appContainer.on('click', '[card-folded]', unfoldCard);
+  function expandCard(){
+    $(this).animate({height:'100px'}, 100);
+  }
+
+  //appContainer.on('click', '[card-cancel]', cardEditCancel);
+  //appContainer.on('click', '[button-card-save]', saveCard);
+  //appContainer.on('click', '[button-card-edit]', editCard);
+  //appContainer.on('click', '[button-card-update]', cardUpdate);
+  appContainer.on('click', '[card]', expandCard);
 });
