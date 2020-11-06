c53c099381829a1685cc97f0ee03df2d5663e916
Luke1024 <chajdas.lukasz@gmail.com>
2020-11-06 17:01:34 +0100

    modified card menu scripting / remove menu modified

--- webUI/script.js
+++ webUI/script.js
@@ -3,6 +3,9 @@ $(document).ready(function(){
   let cardContainer = $("[card-container]");
   let cardTemplate = $("[card-area-template]");
 
+  //new button
+  let newButton = $("[button-card-new]");
+
   //card variables
   let card;
   let buttonAreaInvisible;
@@ -11,6 +14,8 @@ $(document).ready(function(){
   let taskNameParagraph;
   let buttonCardMark;
   let buttonCardRemove;
+  let buttonCardRemoveSecondary;
+  let removeMenu;
 
   let visibleInExpandedMode;
   let taskNameForm;
@@ -23,6 +28,7 @@ $(document).ready(function(){
   let buttonCardAbort;
   let buttonCardFold;
   let buttonCardCancel;
+  let blankWarning;
 
   console.log("execution");
   loadLayout();
@@ -46,6 +52,8 @@ $(document).ready(function(){
     taskNameParagraph = card.find('[task-name-paragraph]');
     buttonCardMark = card.find('[button-card-mark]');
     buttonCardRemove = card.find('[button-card-remove]');
+    buttonCardRemoveSecondary = card.find('[button-card-remove-secondary]');
+    removeMenu = card.find('[remove-menu]');
 
     visibleInExpandedMode = card.find('[visible-in-expanded-mode]');
     taskNameForm = card.find('[task-name-form]');
@@ -58,31 +66,43 @@ $(document).ready(function(){
     buttonCardAbort = card.find('[button-card-abort]');
     buttonCardFold = card.find('[button-card-fold]');
     buttonCardCancel = card.find('[button-card-cancel]');
+    blankWarning = card.find('[blank-warning]');
   }
 
   function processToSavedMode() {
     taskNameForm.fadeOut(200);
     taskDescriptionForm.fadeOut(200);
+    taskNameParagraph.removeClass("hidden");
+    taskDescriptionParagraph.removeClass("hidden");
 
     buttonCardUpdate.addClass("hidden");
     buttonCardAbort.addClass("hidden");
 
     buttonCardEdit.removeClass("hidden");
-    buttonCardAbort.removeClass("hidden");
+    buttonCardFold.removeClass("hidden");
 
-    buttonCardMark.removeClass("hidden");
-    buttonCardRemove.removeClass("hidden");
+    buttonCardMark.fadeIn(200);
+    buttonCardRemove.fadeIn(200);
 
     visibleInExpandedMode.fadeOut(200);
     card.delay(200).animate({height:'30px'});
     buttonAreaInvisible.removeClass("hidden");
   }
 
+  function cardRemove(){
+    loadCardObjectsToGlobalVariables($(this));
+    card.remove();
+  }
+
   function cardUpdate(){
     loadCardObjectsToGlobalVariables($(this));
-    taskNameParagraph.text(taskNameForm.val());
-    taskDescriptionParagraph.text(taskDescriptionForm.val());
-    processToSavedMode();
+    if(taskNameForm.val().length>0){
+      taskNameParagraph.text(taskNameForm.val());
+      taskDescriptionParagraph.text(taskDescriptionForm.val());
+      processToSavedMode();
+    } else {
+      informAboutBlankName();
+    }
   }
 
   function cardAbort(){
@@ -95,14 +115,14 @@ $(document).ready(function(){
   function cardEdit(){
     loadCardObjectsToGlobalVariables($(this));
     buttonCardMark.fadeOut(200);
-    buttonCardRemove.fadeOut(200);
 
     buttonCardEdit.addClass("hidden");
     buttonCardUpdate.removeClass("hidden");
     buttonCardFold.addClass("hidden");
     buttonCardAbort.removeClass("hidden");
 
-
+    taskNameParagraph.addClass("hidden");
+    taskDescriptionParagraph.addClass("hidden");
     taskNameForm.fadeIn(200);
     taskDescriptionForm.fadeIn(200);
 
@@ -119,7 +139,15 @@ $(document).ready(function(){
 
   function saveCard(){
     loadCardObjectsToGlobalVariables($(this));
+    
+    if(taskNameForm.val().length>0){
+      processWithCardSaving();
+    } else {
+      informAboutBlankName();
+    }
+  }
 
+  function processWithCardSaving(){
     taskNameParagraph.text(taskNameForm.val());
     taskDescriptionParagraph.text(taskDescriptionForm.val());
 
@@ -128,15 +156,30 @@ $(document).ready(function(){
     buttonCardCancel.addClass("hidden");
     buttonCardFold.removeClass("hidden");
     buttonCardMark.removeClass("hidden");
-    buttonCardRemove.removeClass("hidden");
 
     taskNameForm.fadeOut(200);
     taskDescriptionForm.fadeOut(400);
+    taskNameParagraph.removeClass("hidden");
+    taskDescriptionParagraph.removeClass("hidden");
 
     visibleInExpandedMode.delay(400).fadeOut(200);
     card.delay(600).animate({height:'30px'});
 
     buttonAreaInvisible.removeClass("hidden");
+    addAuthorizationToCardRemoving();
+  }
+
+  function addAuthorizationToCardRemoving(){
+    buttonCardRemove.addClass("hidden");
+    buttonCardRemoveSecondary.removeClass("hidden");    
+  }
+
+  function toggleRemoveMenu() {
+    removeMenu.fadeToggle(200);
+  }
+
+  function informAboutBlankName(){
+    blankWarning.fadeIn(200).delay(500).fadeOut(2000);
   }
 
   function expandCard(){
@@ -146,6 +189,10 @@ $(document).ready(function(){
     visibleInExpandedMode.delay(200).fadeIn(200);
   }
 
+  appContainer.on('click', '[button-card-remove]', cardRemove);
+  appContainer.on('click', '[button-card-remove-secondary]', toggleRemoveMenu)
+  appContainer.on('click', '[authorize-remove]', cardRemove);
+  appContainer.on('click', '[cancel-remove]', toggleRemoveMenu);
   appContainer.on('click', '[button-card-update]', cardUpdate);
   appContainer.on('click', '[button-card-abort]', cardAbort);
   appContainer.on('click', '[button-card-edit]', cardEdit);
@@ -153,4 +200,5 @@ $(document).ready(function(){
   appContainer.on('click', '[button-card-save]', saveCard);
   appContainer.on('click', '[button-card-fold]', cardCancelFold);
   appContainer.on('click', '[button-area-invisible]', expandCard);
+  appContainer.on('click', '[button-card-new]', loadCardTemplate);
 });
