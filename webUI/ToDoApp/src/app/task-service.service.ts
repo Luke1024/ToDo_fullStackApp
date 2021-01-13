2e175369df9a5f4b176c5aaa13f88bc0fb5f2877
Luke1024 <chajdas.lukasz@gmail.com>
2021-01-13 15:51:03 +0100

    cards scheme working

--- webUI/ToDoApp/src/app/task-service.service.ts
+++ webUI/ToDoApp/src/app/task-service.service.ts
@@ -21,4 +21,6 @@ export class TaskServiceService {
   getTasks(): Task[] {
     return this.tasks
   }
+
+  
 }
--- webUI/ToDoApp/src/app/tasks/tasks.component.ts
+++ webUI/ToDoApp/src/app/tasks/tasks.component.ts
@@ -16,4 +16,32 @@ export class TasksComponent implements OnInit {
   ngOnInit(): void {
     this.tasks = this.taskService.getTasks()
   }
+
+  save(name:string, description:string){
+    if(!name || !description){ return; }
+
+  }
+
+  delete(task: Task): void {
+    this.tasks = this.tasks.filter(t => t != task);
+  }
+
+  add(): void {
+    let maxNum:number = this.findMaxValue()
+    this.tasks.push({id:maxNum, name:"", description:"",done:false})
+  }
+
+  markDone(task: Task): void {
+    task.done = true
+  }
+
+  private findMaxValue():number {
+    let maxNum:number = 0
+    for(let i=0; i<this.tasks.length; i++){
+      if(this.tasks[i].id > maxNum){
+        maxNum = this.tasks[i].id
+      }
+    }
+    return maxNum
+  }
 }
