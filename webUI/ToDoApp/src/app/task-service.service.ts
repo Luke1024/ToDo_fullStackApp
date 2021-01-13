78155c88e6643008fc850446fe31fccb51495006
Luke1024 <chajdas.lukasz@gmail.com>
2021-01-13 17:41:48 +0100

    sketching task-service implementation

--- webUI/ToDoApp/src/app/task-service.service.ts
+++ webUI/ToDoApp/src/app/task-service.service.ts
@@ -1,26 +1,63 @@
 import { Injectable } from '@angular/core';
 import { Task } from './Task';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
  
 @Injectable({
   providedIn: 'root'
 })
 export class TaskServiceService {
 
-  tasks:Task[] = []
+  private rootUrl = 'http://localhost:8080/toDo'
+  private tokenUrl = '/token'
+  private tasksUrl = '/tasks/'
+  private token:String
+  private tokenLoaded = false
 
-  constructor() {
-    this.generateTasks()
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json'})
   }
 
-  private generateTasks():void {
-    for(var i=0; i<10; i++){
-      this.tasks.push({id:i,name:"Task " + i,description: "Description " + i, done:false})
+  constructor(private http:HttpClient) {}
+
+  
+
+  loadToken(token:String){
+    this.token = token
+    this.tokenLoaded = true
+    
+  }
+
+  getTasks(): Observable<Task[]> {
+    if(this.tokenLoaded){
+      return this.http.get<Task[]>(this.rootUrl + this.tasksUrl)
+      .pipe(catchError(this.handleError<Task[]>('getTasks', [])))
+    } else {
+      this.getToken()
     }
   }
 
-  getTasks(): Task[] {
-    return this.tasks
+  getToken():void {
+    this.http.get<String>(this.rootUrl + this.tokenUrl)
+    .pipe(catchError(this.handleError<String>('getToken', '')))
+    .subscribe(token => this.loadToken(token))
   }
 
-  
+  save() {}
+
+
+
+
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
 }
