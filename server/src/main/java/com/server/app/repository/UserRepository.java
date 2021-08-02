416ee66a949a4e2c2acfb41cf23c59227f118ae6
Luke1024 <chajdas.lukasz@gmail.com>
2021-08-02 13:14:55 +0200

    started preparing for heroku deployment

--- server/src/main/java/com/server/app/repository/UserRepository.java
+++ server/src/main/java/com/server/app/repository/UserRepository.java
@@ -11,7 +11,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
-
     @Query
     Optional<User> findByEmail(@Param("EMAIL")String email);
 
